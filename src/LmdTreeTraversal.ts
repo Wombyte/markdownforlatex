70c0b6b3002ca88f366730b235eca1f1254a26e7
Wombyte <marcuspiele@web.de>
2022-04-13 13:19:02 +0200

    cleanup

---
+++ src/LmdTreeTraversal.ts
@@ -0,0 +1,62 @@
+import { LmdNode, LmdNodeType } from './types'
+
+export default abstract class LmdTreeTraversal {
+	root: LmdNode
+
+	constructor(root: LmdNode) {
+		this.root = root
+	}
+
+	traverse(): void {
+		this.dfs(this.root)
+	}
+
+	dfs(node: LmdNode): void {
+		switch (node.type) {
+			case 'root':
+				this.visitRoot(node)
+				break
+			case 'section':
+				this.visitSection(node)
+				break
+			case 'definition':
+				this.visitDefinition(node)
+				break
+			case 'note':
+				this.visitNote(node)
+				break
+			case 'image':
+				this.visitImage(node)
+				break
+		}
+		node.children.forEach((n) => this.dfs(n))
+		switch (node.type) {
+			case 'root':
+				this.afterRoot(node)
+				break
+			case 'section':
+				this.afterSection(node)
+				break
+			case 'definition':
+				this.afterDefinition(node)
+				break
+			case 'note':
+				this.afterNote(node)
+				break
+			case 'image':
+				this.afterImage(node)
+				break
+		}
+	}
+
+	visitRoot(node: LmdNode): void {}
+	afterRoot(node: LmdNode): void {}
+	visitSection(node: LmdNode): void {}
+	afterSection(node: LmdNode): void {}
+	visitDefinition(node: LmdNode): void {}
+	afterDefinition(node: LmdNode): void {}
+	visitNote(node: LmdNode): void {}
+	afterNote(node: LmdNode): void {}
+	visitImage(node: LmdNode): void {}
+	afterImage(node: LmdNode): void {}
+}
